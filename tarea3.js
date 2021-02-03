d277ffa458d0a58d239913d0e8608d4dd9b373b2
ckyonen <73669947+ckyonen@users.noreply.github.com>
2021-02-03 17:49:20 -0300

    Tarea
    Tarea3

---
+++ tarea3.js
@@ -0,0 +1,17 @@
+
+
+
+
+    function Calculo() {
+
+        var Iva = 0.19;
+        var Number1=parseInt(document.getElementById('Num1').value);
+
+        var Result;
+        Result = Number1 + (Number1 * Iva);
+        alert("El Resultado es "+Result);
+
+
+    }
+
+
