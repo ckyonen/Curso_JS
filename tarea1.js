f69605a7cca3e4818140c5ee0a52a80d067d5166
ckyonen <73669947+ckyonen@users.noreply.github.com>
2021-02-04 15:48:02 -0300

    Actualizar Tarea con todas las Preguntas JS
    .

--- tarea1.js
+++ tarea1.js
@@ -1,4 +1,5 @@
 
+// Pregunta 1
 
 var numero = prompt("Introduce un número entero");
 var resultado = parImpar(numero);
@@ -13,3 +14,82 @@ function parImpar(numero) {
         return "impar";
     }
 }
+
+// Pregunta 2
+
+var valor = prompt("Introduce una letra");
+var busqueda = "abcdefghijklmnñopqrstuvwxyz";
+var resultado = buscarString(valor);
+
+
+
+alert("La posicion es: "+resultado);
+
+
+
+function buscarString(valor) {
+
+     return busqueda.indexOf(valor)
+
+
+}
+
+
+// Pregunta 3
+
+    function Calculo() {
+
+        var Iva = 0.19;
+        var Number1=parseInt(document.getElementById('Num1').value);
+
+        var Result;
+        Result = Number1 + (Number1 * Iva);
+        alert("El Resultado es "+Result);
+
+
+    }
+
+
+// Pregunta 4
+
+var nombre = "Julio";
+var min = 1;
+var max = 5;
+
+getRandomArbitrary(min,max);
+
+function getRandomArbitrary(min, max) {
+
+
+    var resultado;
+
+    resultado = Math.random() * (max - min) + min;
+    redondear = Math.round(resultado);
+
+    console.log(nombre+" tiró un dado y salió "+redondear);
+
+}
+
+
+// Pregunta 5
+
+
+function Session() {
+
+
+
+
+    var l_nombre= document.getElementById('nombre').value ;
+    var l_edad=parseInt(document.getElementById('edad').value);
+
+
+
+
+
+    sessionStorage.setItem(l_nombre, l_edad);
+    localStorage.setItem(l_nombre, l_edad);
+
+}
+
+
+
